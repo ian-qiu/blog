5c676e86a8b694391f2a073ec689d1b69164a8aa
ian-qiu <qiuyiyi2006@163.com>
2013-09-12 16:36:57 +0800

    change port

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+#!/bin/env node
 var express = require("express");
 var app = express();
+
+var ejs = require('ejs');
+
+app.use('/static',express.static(__dirname + "/static"));
+app.set("views",__dirname + '/view/');
+app.set("view engine","ejs");
+//app.engine("html",require('ejs').renderFile);
+
 app.get('/',function(req,res){
-	res.end("Hello express!");
+	ejs.renderFile('view/frame.html',{},function(err,html){
+		if(err){
+			console.error(err);
+		}else{
+			res.end(html);
+		}
+	});
 });
-app.listen(3000);
+
+app.get('/index.html',function(req,res){
+	app.render('frame',{},function(err,html){
+		if(err){
+                        console.error(err);
+                }else{
+			res.setHeader("Content-Type","text/html");
+                        res.end(html);
+                }
+	});
+})
+app.listen(3001);
